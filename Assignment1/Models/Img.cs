7084b55623fd1b7285e5f419ef1e9d516eaf4982
Boti21 <botibencsik@gmail.com>
2025-03-02 19:12:54 +0100

    File parsing complete

--- Assignment1/Models/Img.cs
+++ Assignment1/Models/Img.cs
@@ -4,62 +4,25 @@ using System.IO;
 
 namespace Assignment1.Models;
 
-//Console.WriteLine("Hello, World!");
-//var fp = new FileInfo("../../../hello.txt");
-//var path = "/Users/boti/Uni/SDU/Semester6/AOOP/assignment_test/FileReading/hello.txt";
-//string readText = File.ReadAllText(path);
-
-//Console.WriteLine(readText);
-
-//File.WriteAllText(path, "another Hello world");
-
-//Console.WriteLine(File.ReadAllText(path));
-/*
-FileStream fp = File.Open("/Users/boti/Uni/SDU/Semester6/AOOP/assignment_test/FileReading/hello.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-Console.WriteLine(fp.Position);
-fp.Position = 0;
-
-var readByte = fp.ReadByte();
-Console.WriteLine(readByte);
-Console.WriteLine(fp.Position);
-
-
-fp.Position = 0;
-
-readByte = fp.ReadByte();
-Console.WriteLine(readByte);
-Console.WriteLine(fp.Position);
-
-fp.Close();
-
-string FileText = File.ReadAllText("/Users/boti/Uni/SDU/Semester6/AOOP/assignment_test/FileReading/hello.txt");
-Console.WriteLine(FileText);
-Console.WriteLine(FileText.Length);
-
-for (int i = 0; i < FileText.Length; i++)
-{
-    Console.WriteLine(FileText[i]);
-    Console.WriteLine("IsHexDigit:");
-    //Console.WriteLine(IsHexDigit(FileText[i]));
-}
-*/
-
-
 public class Img
 {
+    // Public variables that are set at initialization
     public string FilePath { get; init; }
     public string FileName { get; init; }
     
     public string FileContent { get; init; }
     
+    // Matrix dimensions
     public byte RowDimension { get; init; }
     public byte ColumnDimension { get; init; }
     
+    // Matrix to store pixel values
     public List<List<byte>> ImgMatrix { get; init; }
 
+    // Prints parsed matrix to console
     public void DisplayImgMatrix()
     {
-        Console.Write("{");
+        Console.WriteLine("{");
         for (int i = 0; i < this.RowDimension; i++)
         {
             Console.Write("{");
@@ -69,26 +32,35 @@ public class Img
             }
             Console.Write("}\n");
         }
+        Console.WriteLine("}");
     }
     
     
+    // Checks if digit is a valid hexadecimal value
     private bool IsHexDigit(char c)
     {
-    if (c >= '0' && c <= '9')
-    {
-        return true;
+        if (c >= '0' && c <= '9')
+        {
+            return true;
+        }
+        if (c >= 'A' && c <= 'F')
+        {
+            return true;
+        }
+        if (c >= 'a' && c <= 'f')
+        {
+            return true;
+        }
+        return false;
     }
 
-    if (c >= 'A' && c <= 'F')
+    // Converts valid hex digit to it's integer value
+    private byte HexToByte(char hex)
     {
-        return true;
-    }
-
-    if (c >= 'a' && c <= 'f')
-    {
-        return true;
-    }
-    return false;
+        if(hex >= '0' && hex <= '9') return (byte)(hex - '0');
+        if(hex >= 'a' && hex <= 'f') return (byte)(hex - 'a' + 10);
+        if(hex >= 'A' && hex <= 'F') return (byte)(hex - 'A' + 10);
+        throw new ArgumentException();
     }
 
     public Img(string filePath)
@@ -99,40 +71,57 @@ public class Img
         this.FileName = Path.GetFileName(filePath);
         this.ImgMatrix = new List<List<byte>>();
 
-        int dimensions = 2;
-        
+        // List to store only valid hexadecimal digits
         List<byte> hexDigits = new List<byte>();
-
-        for (int i = 0; i < this.FileContent.Length; i++)
+        
+        // Temporary variables for reading process
+        int dimensions = 2;
+        int dimensionsPos = 0;
+        bool dimensionsFound = false;
+        
+        // Counting variable
+        int filePos = 0;
+        while (dimensionsFound == false && filePos < this.FileContent.Length)
         {
-            if (this.IsHexDigit(this.FileContent[i]))
+            if (this.IsHexDigit(this.FileContent[filePos]))
             {
+                // Parsing dimension values
                 if (dimensions == 2)
                 {
                     dimensions--;
-                    this.RowDimension = (byte)Convert.ToInt16(this.FileContent[i].ToString(), 16);
+                    this.RowDimension = HexToByte(this.FileContent[filePos]);
                 } else if (dimensions == 1)
                 {
                     dimensions--;
-                    this.ColumnDimension = (byte)Convert.ToInt16(this.FileContent[i].ToString(), 16);
+                    this.ColumnDimension = HexToByte(this.FileContent[filePos]);
                 }
-                hexDigits.Add(Convert.ToByte(this.FileContent[i].ToString(), 16));
+                else
+                {
+                    // Dimensions found
+                    dimensionsPos = filePos;
+                    dimensionsFound = true;
+                }
             }        
+            filePos++;
+        } 
+
+        // Extracting valid hex digits of the file after the dimensions
+        for (int i = dimensionsPos; i < this.FileContent.Length; i++)
+        {
+            if (this.IsHexDigit(this.FileContent[i]))
+            {
+                hexDigits.Add(HexToByte(this.FileContent[i]));
+            }
         }
 
+        // Adding values of pixels into matrix
         for (int i = 0; i < this.RowDimension; i++)
         {
             this.ImgMatrix.Add(new List<byte>());
             for (int j = 0; j < this.ColumnDimension; j++)
             {
-                this.ImgMatrix[i].Add(hexDigits[(i * this.RowDimension) + j]);
+                this.ImgMatrix[i].Add(hexDigits[(i * this.ColumnDimension) + j]);
             }
         }
     }
-
-
-
-
 }
-
-//Img img = new Img("/Users/boti/Uni/SDU/Semester6/AOOP/assignment_test/FileReading/smile.b2img.txt");
