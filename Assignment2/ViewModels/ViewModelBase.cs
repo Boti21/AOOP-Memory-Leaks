02d984041478101fc21fbbd5a9e503a62ee10b38
Balage <basza22@student.sdu.dk>
2025-03-16 18:28:42 +0100

    Initial commit for Assignment 2

---
+++ Assignment2/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Assignment2.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Assignment2/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Assignment2.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
