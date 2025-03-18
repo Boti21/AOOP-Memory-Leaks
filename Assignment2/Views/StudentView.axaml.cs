4107e95e0cdbb007014eeb98d53a16d74c3373d9
Boti21 <botibencsik@gmail.com>
2025-03-18 14:55:54 +0100

    more progress

---
+++ Assignment2/Views/StudentView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Assignment2.Views;
+
+public partial class StudentView : UserControl
+{
+    public StudentView()
+    {
+        InitializeComponent();
+    }
+}
