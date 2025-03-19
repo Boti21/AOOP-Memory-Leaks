6d7d5d50a14c09b650f6fd02bc4e9537ebe68d21
PneciL <118889994+PneciL@users.noreply.github.com>
2025-03-19 22:35:05 +0100

    Merge RealMenTestInProduction
    The completion of the merge of the branch RealMenTestInProduction.
    No issues seem to be present

--- Assignment2/Models/MainWindowModel.cs
+++ Assignment2/Models/MainWindowModel.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Assignment2.Models;
 
@@ -96,7 +97,8 @@ public class MainWindowModel
 
     public int create_subject (string iname, string idetails, string iteacher) {
         fetch_data();
-        Teacher current_teacher = (Teacher)users.Find(user => user.username == iteacher);
+        // Teacher current_teacher = (Teacher)users.Find(user => user.username == iteacher);
+        Teacher current_teacher = (Teacher)current_user;
         if (current_teacher == null || current_teacher is not Teacher) return -1;
 
         if (subjects.Find(subject => subject.name.Trim() == iname.Trim()) != null) {
@@ -130,9 +132,76 @@ public class MainWindowModel
         }
         
         Student current_student = (Student)current_user;
+        uint? enrolled = current_student.enrolledSubjects.Find(id => id == isubject.id);
+        if (enrolled != null) {
+            Console.WriteLine ("Already enrolled in course");
+            return -1;
+        }
         isubject.studentsEnrolled.Add(current_student.id);
         if (current_student.enrolledSubjects == null) current_student.enrolledSubjects = new List<uint>();
+
         current_student.enrolledSubjects.Add(isubject.id);
+
+        push_data();
+
+        return 0;
+    }
+
+    public int delete_subject (string iname) {
+        fetch_data();
+        if (current_user is not Teacher) {
+            Console.WriteLine("You do not have the permissions to delete a subject");
+            return -1;
+        }
+        Subject rsubject = subjects.Find(subject => subject.name == iname);
+        if (rsubject == null) {
+            Console.WriteLine("No such subject");
+            return -1;
+        }
+
+        subjects.Remove(rsubject);
+
+        foreach (var user in users) {
+            if (user is Teacher teacher && teacher.subjects != null) {
+                teacher.subjects.RemoveAll(s => s == rsubject.id); 
+            }
+        }
+            
+        foreach (var user in users) {
+            if (user is Student student && student.enrolledSubjects != null) {
+                student.enrolledSubjects.RemoveAll(s => s == rsubject.id);
+            }
+        }  
+
+        push_data();
+
+        return 0;
+    }
+ 
+    public int drop_subject (string iname) {
+        fetch_data();
+        if (current_user is not Student) {
+            Console.WriteLine("You cannot drop courses as a teacher");
+            return -1;
+        }
+
+        Student current_student = (Student)current_user;
+
+        Subject rsubject = subjects.Find(subject => subject.name == iname);
+        if (rsubject == null) {
+            Console.WriteLine("No such course exists");
+            return -1;
+        }
+
+        uint? enrollment = current_student.enrolledSubjects.Find(id => id == rsubject.id);
+
+        if (enrollment == null) {
+            Console.WriteLine("You are not enrolled in such a course");
+            return -1;
+        }
+
+        current_student.enrolledSubjects.Remove(rsubject.id);
+        rsubject.studentsEnrolled.Remove(current_student.id);
 
         push_data();
 
@@ -140,6 +209,12 @@ public class MainWindowModel
     }
  
     private void fetch_data () {
+        User tmp; // Save current user otherwise fetch results in dangling pointer
+        if (current_user is Teacher) tmp = new Teacher();
+        else tmp = new Student();
+
+        tmp = current_user;
+
         data = File.ReadAllText(filePath); 
         if (!string.IsNullOrWhiteSpace(data)) {
             modelData = JsonSerializer.Deserialize<ModelData>(data, jsonOptions) ?? new ModelData();
@@ -148,6 +223,7 @@ public class MainWindowModel
             no_users = (uint)modelData.users.Count;
             no_subjects = (uint)modelData.subjects.Count;
         } 
+        current_user = tmp;
     }
  
     private void push_data () {
