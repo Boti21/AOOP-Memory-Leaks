d55f8403b354e64daed9d845f0ed073f006412b9
Balage <basza22@student.sdu.dk>
2025-03-30 21:19:33 +0200

    LiveCharts2 doesn't work even with official code

--- Assignment3/ViewModels/SelectionWindowViewModel.cs
+++ Assignment3/ViewModels/SelectionWindowViewModel.cs
@@ -1,20 +1,15 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using SkiaSharp;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using Assignment3.Views;
 
 namespace Assignment3.ViewModels
 {
     public class SelectionWindowViewModel : INotifyPropertyChanged
     {
-        public SelectionWindowViewModel()
-        {
-            Headers = new ObservableCollection<string>
-            {
-                "Header 1",
-                "Header 2",
-                "Header 3"
-            };
-        }
-
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged(string propertyName)
@@ -62,5 +57,54 @@ namespace Assignment3.ViewModels
                 }
             }
         }
+        
+        public ObservableCollection<dynamic> SelectedData { get; set; }
+        public SelectionWindowViewModel()
+        {
+            Headers = new ObservableCollection<string>
+            {
+                "Header 1",
+                "Header 2",
+                "Header 3"
+            };
+            SelectedData = new ObservableCollection<dynamic>
+            {
+                // 1.0m, 2.0m, 3.0m
+                "bruh",
+                "I woke up",
+                "in a new Bugatti"
+            };
+        }
+        public ISeries[] Series { get; set; } =
+        [
+            new ColumnSeries<double>
+            {
+                Name = "Mary",
+                Values = [2, 5, 4]
+            },
+            new ColumnSeries<double>
+            {
+                Name = "Ana",
+                Values = [3, 1, 6]
+            }
+        ];
+
+        public Axis[] XAxes { get; set; } =
+        [
+            new Axis
+            {
+                Labels = ["Category 1", "Category 2", "Category 3"],
+                LabelsRotation = 0,
+                SeparatorsPaint = new SolidColorPaint(new SKColor(200, 200, 200)),
+                SeparatorsAtCenter = false,
+                TicksPaint = new SolidColorPaint(new SKColor(35, 35, 35)),
+                TicksAtCenter = true,
+                // By default the axis tries to optimize the number of 
+                // labels to fit the available space, 
+                // when you need to force the axis to show all the labels then you must: 
+                ForceStepToMin = true, 
+                MinStep = 1 
+            }
+        ];
     }
 }
